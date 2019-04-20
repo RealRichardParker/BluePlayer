7f30e5ce31fa1e9032129af718c482fcc49bcf79
EC2 Default User <ec2-user@ip-172-31-34-118.us-east-2.compute.internal>
2019-04-20 06:43:22 +0000

    more env testing

--- music_app.py
+++ music_app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import os
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return os.getenv('AZURE_STORAGE_KEY', 'NO KEY')
+    return render_template("index.html", data=os.getenv('AZURE_STORAGE_KEY', 'NO KEY'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
